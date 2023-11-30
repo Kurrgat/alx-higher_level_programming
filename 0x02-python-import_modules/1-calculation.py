898b73f8d2fb0de8ff0815021b56a39a7d712c83
Kurrgat <mrcykj@gmail.com>
2023-11-30 05:40:18 -0800

    Write a program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    """Print the sum, difference, multiple, and quotient of 10 and 5."""
-    # Import the required functions from calculator_1.py
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
-    # Assign values to variables a and b
     a = 10
     b = 5
 
-    # Perform addition and print the result
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-    # Perform subtraction and print the result
     print("{} - {} = {}".format(a, b, sub(a, b)))
-
-    # Perform multiplication and print the result
     print("{} * {} = {}".format(a, b, mul(a, b)))
-
-    # Perform division and print the result
     print("{} / {} = {}".format(a, b, div(a, b)))
