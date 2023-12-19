c14e7278d4aa8e3838bf4bc2b9fb276e0208d676
Kurrgat <mrcykj@gmail.com>
2023-12-19 10:59:34 -0800

    ByteCode -> Python #5

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""class MagicClass """
+
+
+class MagicClass:
+    """set up magic class"""
+    
+    def __init__(self, radius):
+        """ write docstring """
+
+        if not isinstance(radius, (int, float)):
+            raise TypeError('radius must be a number')
+
+        self._MagicClass__radius = radius
+
+    def area(self):
+        """again with docstring"""
+        
+        return self._MagicClass__radius**2 * math.pi
+
+    def circumference(self):
+        """another docstring"""
+        
+        return 2 * math.pi * self._MagicClass__radius
