87515162cf25d87d3f1c04e77466c06f133d6d07
Kurrgat <mrcykj@gmail.com>
2023-12-19 11:04:04 -0800

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
 
-"""class MagicClass """
+"""Define a MagicClass matching exactly a bytecode provided by Holberton."""
+
+import math
 
 
 class MagicClass:
-    """set up magic class"""
-    
-    def __init__(self, radius):
-        """ write docstring """
+    """Represent a circle."""
 
-        if not isinstance(radius, (int, float)):
-            raise TypeError('radius must be a number')
+    def __init__(self, radius=0):
+        """Initialize a MagicClass.
 
-        self._MagicClass__radius = radius
+        Arg:
+            radius (float or int): The radius of the new MagicClass.
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
     def area(self):
-        """again with docstring"""
-        
-        return self._MagicClass__radius**2 * math.pi
+        """Return the area of the MagicClass."""
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """another docstring"""
-        
-        return 2 * math.pi * self._MagicClass__radius
+        """Return The circumference of the MagicClass."""
+        return (2 * math.pi * self.__radius)
