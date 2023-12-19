8169a078d246cfa8cc46e7e8cef492946356cafd
Kurrgat <mrcykj@gmail.com>
2023-12-19 10:29:30 -0800

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,45 +1,25 @@
 #!/usr/bin/python3
-"""
-Module containing classes Node and SinglyLinkedList.
-"""
+
+"""A class Node that defines a node of a singly linked list """
 
 
 class Node:
-    """
-    Class Node for a singly linked list node.
-    """
-    def __init__(self, data, next_node=None):
-        """
-        Initializes a new instance of the Node class.
+    """"defines a node"""
 
-        Parameters:
-            - data: The data for the node (must be an integer).
-            - next_node: The next node in the linked list (default is None).
-        """
+    def __init__(self, data, next_node=None):
+        """initializes the node with instance variables"""
+
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """
-        Getter method for the data attribute.
+        """gets data attribute"""
 
-        Returns:
-            int: The data of the node.
-        """
         return self.__data
 
     @data.setter
     def data(self, value):
-        """
-        Setter method for the data attribute.
-
-        Parameters:
-            - value: The new data value.
-
-        Raises:
-            - TypeError: If value is not an integer.
-        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         else:
@@ -47,25 +27,10 @@ class Node:
 
     @property
     def next_node(self):
-        """
-        Getter method for the next_node attribute.
-
-        Returns:
-            Node: The next node in the linked list.
-        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """
-        Setter method for the next_node attribute.
-
-        Parameters:
-            - value: The new next_node value.
-
-        Raises:
-            - TypeError: If value is not a Node object.
-        """
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         else:
@@ -73,64 +38,30 @@ class Node:
 
 
 class SinglyLinkedList:
-    """
-    Class SinglyLinkedList for a singly linked list.
-    """
+    """defines a singly linked list"""
+
     def __init__(self):
-        """
-        Initializes a new instance of the SinglyLinkedList class.
-        """
         self.head = None
 
     def sorted_insert(self, value):
-        """
-        Inserts a new Node into the correct sorted position in the list
-        (increasing order).
-
-        Parameters:
-            - value: The value to insert.
-        """
         new_node = Node(value)
-        if self.head is None or self.head.data >= value:
+        if self.head is None or self.head.data >= new_node.data:
             new_node.next_node = self.head
             self.head = new_node
         else:
             current = self.head
-            while current.next_node is not None and
-            current.next_node.data < value:
+            while (
+                current.next_node is not None
+                and current.next_node.data < new_node.data
+            ):
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
 
     def __str__(self):
-        """
-        Returns a string representation of the linked list.
-
-        Returns:
-            str: The string representation of the linked list.
-        """
         result = ""
         current = self.head
-        while current is not None:
+        while current:
             result += str(current.data) + "\n"
             current = current.next_node
         return result.rstrip("\n")
-
-
-if __name__ == "__main__":
-    # Test the SinglyLinkedList class
-    SinglyLinkedList = __import__('100-singly_linked_list').SinglyLinkedList
-
-    sll = SinglyLinkedList()
-    sll.sorted_insert(2)
-    sll.sorted_insert(5)
-    sll.sorted_insert(3)
-    sll.sorted_insert(10)
-    sll.sorted_insert(1)
-    sll.sorted_insert(-4)
-    sll.sorted_insert(-3)
-    sll.sorted_insert(4)
-    sll.sorted_insert(5)
-    sll.sorted_insert(12)
-    sll.sorted_insert(3)
-    print(sll)
