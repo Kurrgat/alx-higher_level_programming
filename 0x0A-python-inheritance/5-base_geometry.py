7326a7a2ea69fcd244ff524eb45bce4000953684
Kurrgat <mrcykj@gmail.com>
2024-01-08 06:22:18 -0800

    Write an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines an empty class BaseGeometry representing the base geometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class representing the base geometry.
+    """
+    pass
