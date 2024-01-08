d9b046902a4451450484baff3cd006dc7cb756c5
Kurrgat <mrcykj@gmail.com>
2024-01-08 05:44:38 -0800

    Write a function that returns True if the object is an instance of, or if the object is an instance of a class that inherited from, the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python 3
+"""
+checks if an object is an instance of, or inherited from, a specified class.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if the object is an instance of, or inherited from,
+    the specified class;
+    otherwise, False.
+
+    Args:
+        obj: Object to check.
+        a_class: Class to compare against
+    """
+    return isinstance(obj, a_class)
