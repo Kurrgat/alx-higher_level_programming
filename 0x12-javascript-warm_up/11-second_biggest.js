42734f4657e1231ca0b4f6cf705221297beeea4b
Kurrgat <mrcykj@gmail.com>
2024-03-12 00:26:17 +0300

    Task 11: Write a script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@
 const args = process.argv.slice(2);
 
 if (args.length <= 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    const sortedArgs = args.map(Number).sort((a, b) => b - a);
-    console.log(sortedArgs[1]);
+  const sortedArgs = args.map(Number).sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
