b43de81aeac58e77f1ac5e8cf7c18a2ed802ed9d
Kurrgat <mrcykj@gmail.com>
2024-03-17 23:19:27 +0300

    Task 0: Write a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        cursor = conn.cursor()
+
+        # Query to select all states from the states table
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetch all rows and display results
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+        # Close cursor and connection
+        cursor.close()
+        conn.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
