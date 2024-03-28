12d3b09ac15505e35205da8edfe7a47c3c0923f6
Kurrgat <mrcykj@gmail.com>
2024-03-28 23:38:21 +0300

    Task 6: Write a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email, sends a POST request, and displays the body of the response"""
+"""Takes in a URL and an email, sends a POST request,
+and displays the body of the response"""
 import requests
 import sys
 
